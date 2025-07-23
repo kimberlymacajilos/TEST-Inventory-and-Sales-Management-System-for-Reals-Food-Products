3a9ba92c8fc0e4846438c5f8e8cd5b2348b0ab1d
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2025-07-24 00:05:31 +0800

    Update database settings to use MySQL and register additional models in admin

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'projectsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reals_db',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
