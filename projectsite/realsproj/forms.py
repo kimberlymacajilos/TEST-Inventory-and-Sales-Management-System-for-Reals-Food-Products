647f0ff02e855ade72ef7b492889d0a47980bacf
usernamewin <winonalvarez@gmail.com>
2025-08-16 19:11:51 +0800

    add templates for managing raw materials and history log
    - created `historylog_list.html` for displaying history logs with pagination
    - added `rawmaterial_add.html` for adding new raw materials
    - implemented `rawmaterial_delete.html` for confirming deletion of raw materials
    - developed `rawmaterial_edit.html` for editing existing raw materials
    - created `rawmaterial_list.html` for listing raw materials with action buttons for edit and delete

--- projectsite/realsproj/forms.py
+++ projectsite/realsproj/forms.py
@@ -1,8 +1,18 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Products
+from .models import Products, RawMaterials, HistoryLog
 
 class ProductsForm(ModelForm):
     class Meta:
         model = Products
+        fields = "__all__"
+
+class RawMaterialsForm(ModelForm):
+    class Meta:
+        model = RawMaterials
+        fields = "__all__"
+
+class HistoryLogForm(ModelForm):
+    class Meta:
+        model = HistoryLog
         fields = "__all__"
--- projectsite/realsproj/views.py
+++ projectsite/realsproj/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from realsproj.forms import ProductsForm
-from realsproj.models import Products
+from realsproj.forms import ProductsForm, RawMaterialsForm, HistoryLogForm
+from realsproj.models import Products, RawMaterials, HistoryLog
 
 class HomePageView(ListView):
     model = Products
@@ -32,4 +32,37 @@ class ProductsDeleteView(DeleteView):
     model = Products
     template_name = 'prod_delete.html'
     success_url = reverse_lazy('products')
+
+class RawMaterialsList(ListView):
+    model = RawMaterials
+    context_object_name = 'rawmaterials'
+    template_name = "rawmaterial_list.html"
+    paginate_by = 10
+
+class RawMaterialsCreateView(CreateView):
+    model = RawMaterials
+    form_class = RawMaterialsForm
+    template_name = 'rawmaterial_add.html'
+    success_url = reverse_lazy('rawmaterials')
+
+class RawMaterialsUpdateView(UpdateView):
+    model = RawMaterials
+    form_class = RawMaterialsForm
+    template_name = 'rawmaterial_edit.html'
+    success_url = reverse_lazy('rawmaterials')
+
+class RawMaterialsDeleteView(DeleteView):
+    model = RawMaterials
+    template_name = 'rawmaterial_delete.html'
+    success_url = reverse_lazy('rawmaterials')
+
+class HistoryLogList(ListView):
+    model = HistoryLog
+    context_object_name = 'historylog'
+    template_name = "historylog_list.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return HistoryLog.objects.all().order_by('-id')  # or '-log_date'
+
 # Create your views here.
