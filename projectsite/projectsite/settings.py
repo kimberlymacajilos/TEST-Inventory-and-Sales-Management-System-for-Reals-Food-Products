54466a7fcf4a01bcf3304e167697a61e9fc8b17f
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2025-09-28 16:33:33 +0800

    enhance search input fields for raw materials and batches

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'reals_local',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432',
     }
