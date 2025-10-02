846051d34e5e1f6c6ddfb0e064df5b364541d69b
jsnodees <202280134@psu.palawan.edu.ph>
2025-10-02 19:23:19 +0800

    update database name in settings to 'reals_local'

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'projectsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'reals_local',
         'USER': 'postgres',
         'PASSWORD': 'reals',
         'HOST': 'localhost',
