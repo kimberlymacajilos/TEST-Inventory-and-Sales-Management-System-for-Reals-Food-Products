14efc38d9660669e6762dfc44162c59633be64b6
jsnodees <202280134@psu.palawan.edu.ph>
2025-10-28 17:52:37 +0800

    added a delete account feature, made a modal to 2fa setup

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -84,26 +84,26 @@ WSGI_APPLICATION = 'projectsite.wsgi.application'
 
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'postgres',
-#        'USER': 'postgres',
-#        'PASSWORD': 'Reals_db_123',
-#        'HOST': 'db.rczsumkmhoxjaycvggzt.supabase.co',
-#        'PORT': '5432',
-#    }
-#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'reals_local',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': 'Reals_db_123',
+       'HOST': 'db.rczsumkmhoxjaycvggzt.supabase.co',
+       'PORT': '5432',
+   }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'reals_local',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
